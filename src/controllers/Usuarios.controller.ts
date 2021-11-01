292e6e90344d7f15dfabb0a77b6b2b968cf2b03b
Luciano Morais <lucianopythonista@gmail.com>
2021-10-31 23:42:32 -0300

    criação de update e nos gets na controller Usuarios

--- src/controllers/Usuarios.controller.ts
+++ src/controllers/Usuarios.controller.ts
@@ -1,40 +1,43 @@
-import { Body, Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParamData, Paramtype, Post, Put } from "@nestjs/common";
 import { Usuario } from "src/models/Usuarios.model";
+import { UsuariosService } from "src/services/Usuarios.service";
 
 @Controller('usuarios')
 export class UsuariosController {
-    
+
+    constructor(private usuariosService: UsuariosService) {}
+
     @Get()
     buscar_todos() {
-
+        try {
+            return this.usuariosService.listar_todos();
+        } catch (error) {
+            return error;
+        }
     }
     
     @Get(':id')
-    buscar_id() {
-
+    buscar_id(@Param() param) {
+        try {
+            return this.usuariosService.listar_id(param.id);
+        } catch (error) {
+            return error;
+        }
     }
     
     @Post()
     adicionar(@Body() usuario: Usuario) {
         try {
-            const novo_usuario = new Usuario(
-                usuario.nome,
-                usuario.tipo,
-                usuario.email,
-                usuario.data_nascimento,
-                usuario.senha
-            );
-            console.log(novo_usuario);
+            this.usuariosService.adicionar(usuario);
+            return usuario;
         } catch (error) {
-            console.log(error)
+            return error;
         }
     }
 
-
-
     @Put(':id')
     atualizar() {
-
+        
     }
 
     @Delete(':id')
