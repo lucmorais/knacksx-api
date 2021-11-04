e9d36c4a4c4138c7bda2785adc79344395445667
Luciano Morais <lucianopythonista@gmail.com>
2021-11-03 23:50:21 -0300

    atualização no módulo principal com controller e service de habilidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HabilidadesController } from './controllers/Habilidades.controller';
 import { UsuariosController } from './controllers/usuarios.controller';
+import { HabilidadesService } from './services/habilidades.service';
 import { UsuariosService } from './services/usuarios.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsuariosController],
-  providers: [AppService, UsuariosService],
+  controllers: [AppController, UsuariosController, HabilidadesController],
+  providers: [AppService, UsuariosService, HabilidadesService],
 })
 export class AppModule {}
