f509d181d0851e0b54b246ca04dc50aec924879c
Luciano Morais <lucianopythonista@gmail.com>
2021-11-03 23:42:20 -0300

    criação das rotas de habilidades, criação da service de habilidades e atualização na model de habilidades

--- src/models/Habilidades.model.ts
+++ src/models/Habilidades.model.ts
@@ -1,13 +1,16 @@
-export class Habilidades {
-    private titulo: string;
-    private descricao: string;
-    private nivel: number;
+export class Habilidade {
+    public id: number;
+    public titulo: string;
+    public descricao: string;
+    public nivel: number;
 
     constructor(
+        id: number,
         titulo: string,
         descricao: string,
         nivel: number
     ) {
+        this.id = id;
         this.titulo = titulo;
         this.descricao = descricao;
         this.nivel = nivel;
