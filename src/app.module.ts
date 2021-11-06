a9a9337f7056cf8c738b6d4187f38caa9070c12a
Luciano Morais <lucianopythonista@gmail.com>
2021-11-06 20:28:01 -0300

    integraçao com mysql e sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HabilidadesController } from './controllers/Habilidades.controller';
 import { UsuariosController } from './controllers/usuarios.controller';
 import { HabilidadesService } from './services/habilidades.service';
 import { UsuariosService } from './services/usuarios.service';
+import { Usuario } from './models/usuarios.model';
+import { Habilidade } from './models/habilidades.model';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.HOST,
+      port: parseInt(process.env.PORTA),
+      username: process.env.USUARIO_BD,
+      password: process.env.SENHA_BD,
+      database: process.env.BD,
+      autoLoadModels: true,
+    }),
+    SequelizeModule.forFeature([Usuario, Habilidade])
+  ],
   controllers: [AppController, UsuariosController, HabilidadesController],
   providers: [AppService, UsuariosService, HabilidadesService],
 })
