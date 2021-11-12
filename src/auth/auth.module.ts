7776774ffc19b4b7749d360b5a3780043359b3dc
Luciano Morais <lucianopythonista@gmail.com>
2021-11-12 12:51:42 -0300

    criação e retorno do jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,19 @@ import { UsuarioService } from "src/usuario/usuario.service";
 import { UsuarioModule } from "src/usuario/usuario.module";
 import { AuthService } from "./auth.service";
 import { LocalStrategy } from "./local.strategy";
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { jwtConstants } from "./constants";
 
 @Module({
-    imports: [UsuarioModule, PassportModule],
-    providers: [AuthService, LocalStrategy]
+    imports: [
+        UsuarioModule, 
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: { expiresIn: '60s' },
+        })
+    ],    
+    providers: [AuthService, LocalStrategy],
+    exports: [AuthService]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { UsuarioService } from "src/usuario/usuario.service";
 
 @Injectable()
 export class AuthService {
 
-    constructor(private usuarioService: UsuarioService) {}
+    constructor(
+        private usuarioService: UsuarioService,
+        private jwtService: JwtService
+    ) {}
 
     async validarUsuario(email: string, senha_usuario: string): Promise<any> {
         const usuario = await this.usuarioService.listar_email(email);
@@ -14,5 +18,13 @@ export class AuthService {
             return result;
         }
         return null;
+    }
+
+    async login(user: any) {
+        const payload = { username: user.username, sub: user.userId };
+        
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 }
